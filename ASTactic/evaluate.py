b7bfb41d0674b7125085d47e0ba4397cd21714cc
Julian Cooper <jelc@stanford.edu>
2023-03-14 16:31:49 -0700

    evaluet args update

--- ASTactic/evaluate.py
+++ ASTactic/evaluate.py
@@ -63,6 +63,13 @@ if __name__ == "__main__":
     parser.add_argument(
         "--hidden_dim", type=int, default=256, help="dimension of the LSTM controller"
     )
+
+    # term encoder
+    parser.add_argument("--model_type", type=str, default="GraphSage")
+    parser.add_argument("--dropout", type=int, default=0.5)
+    parser.add_argument("--num_layers", type=int, default=2)
+    parser.add_argument("--heads", type=int, default=1)
+
     parser.add_argument("--seed", type=int, default=0)
     opts = parser.parse_args()
     log(opts)
