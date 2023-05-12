8e775003030c05d571e836bfb2a44dcf5ee6b62f
pi314ever <pilotflyer824@gmail.com>
2023-05-12 11:27:08 -0700

    Updated stats analyze to include used tactics

--- ASTactic/stats.py
+++ ASTactic/stats.py
@@ -11,7 +11,7 @@ import pandas as pd
 import pickle
 import sys
 
-from extract_proof_steps import parse_goal, filter_env
+from extract_proof_steps import parse_goal, filter_env, tactic2actions
 
 
 def collect(projects, n_cpu):
@@ -47,6 +47,18 @@ def stats(proof):
             }
     steps = []
     for step in proof["steps"]:
+        if step["command"][1] in [
+            "VernacEndProof",
+            "VernacBullet",
+            "VernacSubproof",
+            "VernacEndSubproof",
+        ]:
+            continue
+        tac_str = step["command"][0][:-1]
+        try:
+            actions = tactic2actions(tac_str)
+        except:
+            continue
         if step["goal_ids"]["fg"]:
             g, lc = {}, {}
             goal_id = step["goal_ids"]["fg"][0]
@@ -57,7 +69,14 @@ def stats(proof):
                     "height": item["ast"].height,
                     "n": len(item["x"]),
                 }
-            steps.append({"local_context": lc, "goal": g})
+            steps.append(
+                {
+                    "local_context": lc,
+                    "goal": g,
+                    "actions": actions,
+                    "tactic_str": tac_str,
+                }
+            )
     return proof["name"], {"steps": steps, "env": env}
 
 
@@ -79,21 +98,79 @@ def analyze_project(path):
     return v
 
 
+TACTICS = {
+    "intro",
+    "apply",
+    "auto",
+    "rewrite",
+    "simpl",
+    "unfold",
+    "destruct",
+    "induction",
+    "elim",
+    "split",
+    "assumption",
+    "trivial",
+    "reflexivity",
+    "case",
+    "clear",
+    "subst",
+    "generalize",
+    "exists",
+    "red",
+    "omega",
+    "discriminate",
+    "inversion",
+    "simple_induction",
+    "constructor",
+    "congruence",
+    "left",
+    "right",
+    "ring",
+    "symmetry",
+    "f_equal",
+    "tauto",
+    "revert",
+    "specialize",
+    "idtac",
+    "hnf",
+    "inversion_clear",
+    "exfalso",
+    "cbv",
+    "contradict",
+    "lia",
+    "field",
+    "easy",
+    "cbn",
+    "exact",
+    "intuition",
+    "eauto",
+}
+
+
 def summarize(steps):
     heights = []
     ns = []
+    used_tactics = dict.fromkeys(TACTICS, 0)
     for step in steps:
         for d in step["local_context"].values():
             heights.append(d["height"])
             ns.append(d["n"])
         heights.append(step["goal"]["height"])
         ns.append(step["goal"]["n"])
+        for tac in step["tactic_str"].split():
+            if tac in TACTICS:
+                used_tactics[tac] += 1
     qs = [0.01, 0.25, 0.5, 0.75, 0.99, 1.0]
+    if not heights:
+        heights = [-1]
+    if not ns:
+        ns = [-1]
     h_qs = np.quantile(heights, qs, interpolation="higher")
     n_qs = np.quantile(ns, qs, interpolation="higher")
     h_d = {f"height_p{int(q*100)}": v for q, v in zip(qs, h_qs)}
     n_d = {f"nodes_p{int(q*100)}": v for q, v in zip(qs, n_qs)}
-    return {"n_steps": len(steps), **h_d, **n_d}
+    return {"n_steps": len(steps), **h_d, **n_d, **used_tactics}
 
 
 def parse_args(argv):
